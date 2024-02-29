1c4d1feb8481c5b2396203a43839ef5c8dc4c177
Luis Sanchez <luiasanc.al@gmail.com>
2024-02-29 16:36:04 -0500

    Declaracion de capas, clases y contextos

---
+++ SistemaVenta.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVenta.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
